d5a6c24b023832cc01a46f6f3393d339d4e4715e
MarieVeltri <marie.veltri@condorcet.be>
2024-11-25 21:27:01 +0100

    Add fuctionnalities to add a skier

--- src/be/veltri/dao/SkierDAO.java
+++ src/be/veltri/dao/SkierDAO.java
@@ -10,10 +10,60 @@ public class SkierDAO extends DAO<Skier> {
         super(conn);
     }
     
-    public boolean createDAO(Skier obj)
-	{
-		return false;
-	}
+    public int getNextIdDAO() {
+        String idSql = "SELECT SKIER_SEQ.NEXTVAL FROM DUAL";
+        try (PreparedStatement idPstmt = this.connect.prepareStatement(idSql);
+             ResultSet rs = idPstmt.executeQuery()) {
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1; 
+    }
+    
+    @Override
+    public boolean createDAO(Skier skier) {
+        String personSql = "INSERT INTO Person (id_Person, firstName, lastName, Birthdate) VALUES (PERSON_SEQ.NEXTVAL, ?, ?, ?)";
+        String personIdSql = "SELECT PERSON_SEQ.CURRVAL FROM DUAL";
+        String skierSql = "INSERT INTO Skier (id_skier, skier_phoneNumber, skier_email, id_Person) VALUES (?, ?, ?, ?)";
+        
+        try (PreparedStatement pstmtPerson = this.connect.prepareStatement(personSql);
+             PreparedStatement pstmtPersonId = this.connect.prepareStatement(personIdSql);
+             PreparedStatement pstmtSkier = this.connect.prepareStatement(skierSql)) {
+
+            pstmtPerson.setString(1, skier.getFirstName());
+            pstmtPerson.setString(2, skier.getLastName());
+            pstmtPerson.setDate(3, Date.valueOf(skier.getBirthdate())); 
+            pstmtPerson.executeUpdate();
+            
+            ResultSet rsPersonId = pstmtPersonId.executeQuery();
+            int personId = -1;
+            if (rsPersonId.next()) {
+                personId = rsPersonId.getInt(1);
+            }
+
+            if (personId == -1) {
+                throw new SQLException("Failed to retrieve generated Person ID.");
+            }
+
+			if (skier.getId() == 0) {
+				skier.setId(getNextIdDAO());
+			}
+            pstmtSkier.setInt(1, skier.getId());
+            pstmtSkier.setString(2, skier.getPhoneNumber());
+            pstmtSkier.setString(3, skier.getEmail());
+            pstmtSkier.setInt(4, personId);
+
+            int rowsAffected = pstmtSkier.executeUpdate();
+            return rowsAffected > 0; 
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false; 
+    }
     
     public boolean deleteDAO(Skier obj)
 	{
